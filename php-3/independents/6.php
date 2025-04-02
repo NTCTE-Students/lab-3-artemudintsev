c441e8356dc0bff892290c2dbf7ae72592c16f30
artemudintsev <ydincev.artem@nttek.ru>
2025-04-02 10:48:31 +0500

    Delete php-3 directory

--- php-3/independents/6.php
+++
@@ -1,5 +0,0 @@
-<?php
-$array1 = [1, 2, 3];
-$array2 = [4, 5, 6];
-$mergedArray = array_merge($array1, $array2);
-print_r($mergedArray);
--- php-3/independents/work_7.php
+++
@@ -1,4 +0,0 @@
-<?php
-$string = "Привет, как твои дела?";
-$array = explode(" ", $string);
-print_r($array);
